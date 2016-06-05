1f5974bb0fb6f98338b6d5e79121a08e57e34685
Mikhail Lyubushkin <clipeus@live.com>
2016-06-05 12:42:47 +0300

    C++14 support

--- server/sources/threadpool.h
+++ server/sources/threadpool.h
@@ -30,9 +30,9 @@ public:
     
     // add new work item to the pool
     template<class F, class... Args>
-    decltype(auto) enqueue(F&& f, Args&&... args) //-> std::future<typename std::result_of<F(Args...)>::type>
+    decltype(auto) enqueue(F&& f, Args&&... args)
     {
-        using return_type = typename std::result_of<F(Args...)>::type;
+        using return_type = std::result_of_t<F(Args...)>;
 
         auto task = std::make_shared<std::packaged_task<return_type()> >(std::bind(std::forward<F>(f), std::forward<Args>(args)...));
             
